bb9f824f76d48e86aeeeea2512641b36d2486d4c
kutlu <halimkutlu1@gmail.com>
2024-01-06 01:10:00 +0300

    Auto Login with languages

---
+++ lib/pages/survey.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Survey extends StatefulWidget {
+  const Survey({super.key});
+
+  @override
+  State<Survey> createState() => _SurveyState();
+}
+
+class _SurveyState extends State<Survey> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
--- lib/providers/splashScreenProvider.dart
+++ lib/providers/splashScreenProvider.dart
@@ -1,11 +1,16 @@
-// ignore_for_file: use_build_context_synchronously
+// ignore_for_file: use_build_context_synchronously, avoid_print
 
 import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:patikmobile/models/language.model.dart';
+import 'package:patikmobile/pages/dashboard.dart';
 import 'package:patikmobile/pages/introductionPages/introductionPage1.dart';
 import 'package:patikmobile/pages/login.dart';
+import 'package:patikmobile/providers/dbprovider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:sqflite/sqflite.dart';
 
 class SplashScreenProvider extends ChangeNotifier {
   initData(BuildContext context) async {
@@ -17,9 +22,36 @@ class SplashScreenProvider extends ChangeNotifier {
             MaterialPageRoute(builder: (context) => IntroductionPage1()),
             (Route<dynamic> route) => false);
       } else {
-        Navigator.of(context).pushAndRemoveUntil(
-            MaterialPageRoute(builder: (context) => Login()),
-            (Route<dynamic> route) => false);
+        String? token = prefs.getString("Token");
+        String? currentLanguageLCID = prefs.getString("CurrentLanguageLCID");
+        List<String>? languageList = prefs.getStringList("languageList");
+        if ((token == null) ||
+            (currentLanguageLCID == null) ||
+            (languageList == null)) {
+          Navigator.of(context).pushAndRemoveUntil(
+              MaterialPageRoute(builder: (context) => const Login()),
+              (Route<dynamic> route) => false);
+        } else {
+          var language = await Languages.getLanguagesFromLocalStorageWithLCID(
+              int.parse(currentLanguageLCID));
+          var path = await DbProvider().getDbPath(lngName: language);
+          var pathExist = await File(path).exists();
+          if (pathExist) {
+            var dbProvider = DbProvider();
+            FileDownloadStatus status =
+                await dbProvider.openDbConnection(language);
+            if (status.status) {
+              print("Db local storage üzerinden aktif");
+              Navigator.of(context).pushAndRemoveUntil(
+                  MaterialPageRoute(builder: (context) => const Dashboard()),
+                  (Route<dynamic> route) => false);
+            }
+          } else {
+            Navigator.of(context).pushAndRemoveUntil(
+                MaterialPageRoute(builder: (context) => const Login()),
+                (Route<dynamic> route) => false);
+          }
+        }
       }
     });
 
