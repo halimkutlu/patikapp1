07dbf48c09a19db8bc2c90a95f093c85d53fcd1c
Halim ATAK <halimkutlu1@gmail.com>
2024-01-12 21:28:02 +0300

    dashbord drawer

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -94,12 +94,11 @@ class _DashboardState extends State<Dashboard> {
           // Add a ListView to the drawer. This ensures the user can scroll
           // through the options in the drawer if there isn't enough vertical
           // space to fit everything.
-          child: ListView(
+          child: Column(
             // Important: Remove any padding from the ListView.
-            padding: EdgeInsets.zero,
             children: [
               SizedBox(
-                height: 50.h,
+                height: 43.h,
                 child: DrawerHeader(
                   decoration: BoxDecoration(
                     border: Border(
@@ -133,136 +132,151 @@ class _DashboardState extends State<Dashboard> {
                         ),
                       ),
                       Container(
-                        height: 10.h,
-                        width: 25.w,
-                        decoration: const ShapeDecoration(
-                          color: Color(0xFFD7D6D6),
-                          shape: OvalBorder(),
-                        ),
-                        child: Image.asset(
-                          'lib/assets/avatar.png',
-                          height: 10,
-                          width: 10,
+                        transform: Matrix4.translationValues(0.0, -20.0, 0.0),
+                        child: Column(
+                          children: [
+                            Container(
+                              height: 10.h,
+                              width: 25.w,
+                              decoration: const ShapeDecoration(
+                                color: Color(0xFFD7D6D6),
+                                shape: OvalBorder(),
+                              ),
+                              child: Image.asset(
+                                'lib/assets/avatar.png',
+                                height: 10,
+                                width: 10,
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Center(child: Text(mainProvider.userName)),
+                            ),
+                            Center(
+                                child: Text(
+                              mainProvider.nameLastname,
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            )),
+                            Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Center(
+                                  child: mainProvider.roleid == UserRole.free
+                                      ? Text.rich(
+                                          TextSpan(
+                                            children: [
+                                              TextSpan(
+                                                text: 'Sınırlı kullanım ',
+                                                style: TextStyle(
+                                                  color: Color(0xFF605E5E),
+                                                  fontSize: 14,
+                                                  fontFamily: 'Roboto',
+                                                  fontWeight: FontWeight.w500,
+                                                  height: 0.10,
+                                                ),
+                                              ),
+                                              TextSpan(
+                                                text: 'Ücretsiz sürüm',
+                                                style: TextStyle(
+                                                  color: Color(0xFFE8233D),
+                                                  fontSize: 14,
+                                                  fontFamily: 'Roboto',
+                                                  fontWeight: FontWeight.w400,
+                                                  height: 0.10,
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                          textAlign: TextAlign.center,
+                                        )
+                                      : mainProvider.roleid == UserRole.premium
+                                          ? Text("Premium")
+                                          : mainProvider.roleid == UserRole.qr
+                                              ? Text("QR")
+                                              : mainProvider.roleid ==
+                                                      UserRole.admin
+                                                  ? Text("Admin")
+                                                  : Text(""),
+                                )),
+                            CustomIconButton(
+                              height: 15,
+                              textInlinePadding: 20,
+                              width: 3,
+                              colors: Colors.red,
+                              name: "Satın Al",
+                            )
+                          ],
                         ),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Center(child: Text(mainProvider.userName)),
-                      ),
-                      Center(
-                          child: Text(
-                        mainProvider.nameLastname,
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      )),
-                      Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Center(
-                            child: mainProvider.roleid == UserRole.free
-                                ? Text.rich(
-                                    TextSpan(
-                                      children: [
-                                        TextSpan(
-                                          text: 'Sınırlı kullanım ',
-                                          style: TextStyle(
-                                            color: Color(0xFF605E5E),
-                                            fontSize: 14,
-                                            fontFamily: 'Roboto',
-                                            fontWeight: FontWeight.w500,
-                                            height: 0.10,
-                                          ),
-                                        ),
-                                        TextSpan(
-                                          text: 'Ücretsiz sürüm',
-                                          style: TextStyle(
-                                            color: Color(0xFFE8233D),
-                                            fontSize: 14,
-                                            fontFamily: 'Roboto',
-                                            fontWeight: FontWeight.w400,
-                                            height: 0.10,
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                    textAlign: TextAlign.center,
-                                  )
-                                : mainProvider.roleid == UserRole.premium
-                                    ? Text("Premium")
-                                    : mainProvider.roleid == UserRole.qr
-                                        ? Text("QR")
-                                        : mainProvider.roleid == UserRole.admin
-                                            ? Text("Admin")
-                                            : Text(""),
-                          )),
-                      CustomIconButton(
-                        height: 25,
-                        textInlinePadding: 20,
-                        width: 3,
-                        colors: Colors.red,
-                        name: "Satın Al",
-                      )
                     ],
                   )),
                 ),
               ),
-              MenuItem(
-                  logo: 'lib/assets/user_language.png',
-                  text: "Kullanıcı Dili",
-                  centerWidget: Text(mainProvider.useLanguageName),
-                  onTap: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => SelectLanguage(
-                              dashboard: true,
-                            )));
-                  }),
-              MenuItem(
-                  logo: 'lib/assets/learn_language.png',
-                  text: "Öğrenme Dili",
-                  centerWidget: Text(mainProvider.learnLanguageName),
-                  onTap: () {}),
-              MenuItem(
-                  logo: 'lib/assets/internet.png',
-                  text: "Çevrimdışı Mod",
-                  centerWidget: CustomIconButton(
-                    textSize: 60,
-                    textInlinePadding: 25,
-                    width: 4,
-                    colors: Colors.red,
-                    name: "Satın Al",
-                  ),
-                  onTap: () {}),
-              MenuItem(
-                  logo: 'lib/assets/key.png',
-                  text: "Şifre Değiştir",
-                  centerWidget: Text(""),
-                  onTap: () {}),
-              MenuItem(
-                  logo: 'lib/assets/edit.png',
-                  text: "Profil Adını Değiştir",
-                  centerWidget: Text(""),
-                  onTap: () {}),
-              MenuItem(
-                  logo: 'lib/assets/privacy.png',
-                  text: "Gizlilik Politikası",
-                  centerWidget: Text(""),
-                  onTap: () {}),
-              MenuItem(
-                  logo: 'lib/assets/star_.png',
-                  text: "Uygulamayı deperlendir",
-                  centerWidget: Text(""),
-                  onTap: () {}),
-              MenuItem(
-                  logo: 'lib/assets/mail.png',
-                  text: "Geri Bildirim",
-                  centerWidget: Text(""),
-                  onTap: () {}),
-              MenuItem(
-                  logo: 'lib/assets/mail.png',
-                  text: "Uygulama Hakkında",
-                  centerWidget: Text(""),
-                  onTap: () {
-                    Navigator.of(context).push(
-                        MaterialPageRoute(builder: (context) => AppInfo()));
-                  }),
+              Expanded(
+                  child: ListView(
+                padding: EdgeInsets.only(bottom: 1.h),
+                shrinkWrap: true,
+                children: <Widget>[
+                  MenuItem(
+                      logo: 'lib/assets/user_language.png',
+                      text: "Kullanıcı Dili",
+                      centerWidget: Text(mainProvider.useLanguageName),
+                      onTap: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => SelectLanguage(
+                                  dashboard: true,
+                                )));
+                      }),
+                  MenuItem(
+                      logo: 'lib/assets/learn_language.png',
+                      text: "Öğrenme Dili",
+                      centerWidget: Text(mainProvider.learnLanguageName),
+                      onTap: () {}),
+                  MenuItem(
+                      logo: 'lib/assets/internet.png',
+                      text: "Çevrimdışı Mod",
+                      centerWidget: CustomIconButton(
+                        textSize: 60,
+                        textInlinePadding: 25,
+                        width: 4,
+                        colors: Colors.red,
+                        name: "Satın Al",
+                      ),
+                      onTap: () {}),
+                  MenuItem(
+                      logo: 'lib/assets/key.png',
+                      text: "Şifre Değiştir",
+                      centerWidget: Text(""),
+                      onTap: () {}),
+                  MenuItem(
+                      logo: 'lib/assets/edit.png',
+                      text: "Profil Adını Değiştir",
+                      centerWidget: Text(""),
+                      onTap: () {}),
+                  MenuItem(
+                      logo: 'lib/assets/privacy.png',
+                      text: "Gizlilik Politikası",
+                      centerWidget: Text(""),
+                      onTap: () {}),
+                  MenuItem(
+                      logo: 'lib/assets/star_.png',
+                      text: "Uygulamayı deperlendir",
+                      centerWidget: Text(""),
+                      onTap: () {}),
+                  MenuItem(
+                      logo: 'lib/assets/mail.png',
+                      text: "Geri Bildirim",
+                      centerWidget: Text(""),
+                      onTap: () {}),
+                  MenuItem(
+                      logo: 'lib/assets/mail.png',
+                      text: "Uygulama Hakkında",
+                      centerWidget: Text(""),
+                      onTap: () {
+                        Navigator.of(context).push(
+                            MaterialPageRoute(builder: (context) => AppInfo()));
+                      }),
+                ],
+              ))
             ],
           ),
         ),
