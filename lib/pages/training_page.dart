2dd12afbd663ce19259770ebd4e97208ed0c78db
kutlu <halimkutlu1@gmail.com>
2024-01-17 00:27:10 +0300

    CARD

--- lib/pages/training_page.dart
+++ lib/pages/training_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_card_swiper/flutter_card_swiper.dart';
+import 'package:patikmobile/assets/style/mainColors.dart';
 
 class TrainingPage extends StatefulWidget {
   const TrainingPage({super.key});
@@ -10,6 +12,21 @@ class TrainingPage extends StatefulWidget {
 class _TrainingPageState extends State<TrainingPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: CardSwiper(
+        numberOfCardsDisplayed: 3,
+        backCardOffset: Offset(-25, -40),
+        cardsCount: 4,
+        cardBuilder: (context, index, percentThresholdX, percentThresholdY) =>
+            Container(
+          decoration: BoxDecoration(
+              color: MainColors.primaryColor,
+              borderRadius: BorderRadius.all(Radius.circular(40)),
+              border: Border.all(width: 0.2)),
+          alignment: new Alignment(0, 0),
+        ),
+        isLoop: false,
+      ),
+    );
   }
 }
