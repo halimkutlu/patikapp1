e8ad819784d059e6e85c84fd558d7fd65282cad2
kutlu <halimkutlu1@gmail.com>
2024-01-20 00:01:38 +0300

    audio player

---
+++ lib/pages/games/swipe_card_game.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SwipeCardGame extends StatefulWidget {
+  const SwipeCardGame({super.key});
+
+  @override
+  State<SwipeCardGame> createState() => _SwipeCardGameState();
+}
+
+class _SwipeCardGameState extends State<SwipeCardGame> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
