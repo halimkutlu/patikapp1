1a4d5ef188d350a6215a371a184ef0819db2a8de
kutluhanozbakan <kutluhanozbakan21@gmail.com>
2024-04-06 11:36:24 +0300

    .

--- lib/pages/splashScreen.dart
+++ lib/pages/splashScreen.dart
@@ -56,6 +56,7 @@ import 'package:flutter/material.dart';
 import 'package:patikmobile/assets/style/mainColors.dart';
 import 'package:patikmobile/providers/splashScreenProvider.dart';
 import 'package:provider/provider.dart';
+import 'package:sizer/sizer.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -69,7 +70,7 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     final splashProvider =
         Provider.of<SplashScreenProvider>(context, listen: false);
-    splashProvider.initData(context);
+      splashProvider.initData(context);
 
     super.initState();
   }
@@ -78,195 +79,16 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        clipBehavior: Clip.antiAlias,
         decoration: BoxDecoration(color: MainColors.backgroundColor),
-        child: Stack(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Positioned(
-              left: 0,
-              top: 0,
-              child: Container(
-                width: 390,
-                height: 44,
-                clipBehavior: Clip.antiAlias,
-                decoration: BoxDecoration(color: MainColors.backgroundColor),
-                child: Stack(
-                  children: [
-                    Positioned(
-                      left: 0,
-                      top: 0,
-                      child: Container(width: 390, height: 30),
-                    ),
-                    Positioned(
-                      left: 307,
-                      top: 16,
-                      child: Container(
-                        child: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Container(
-                              width: 20,
-                              height: 14,
-                              child: Stack(children: []),
-                            ),
-                            const SizedBox(width: 4),
-                            Container(
-                              width: 16,
-                              height: 14,
-                              child: Stack(children: []),
-                            ),
-                            const SizedBox(width: 4),
-                            Container(
-                              width: 25,
-                              height: 14,
-                              child: Stack(
-                                children: [
-                                  Positioned(
-                                    left: 2,
-                                    top: 3,
-                                    child: Container(
-                                      width: 19,
-                                      height: 8,
-                                      decoration: ShapeDecoration(
-                                        color: Colors.black,
-                                        shape: RoundedRectangleBorder(
-                                            borderRadius:
-                                                BorderRadius.circular(1)),
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    Positioned(
-                      left: 313,
-                      top: 8,
-                      child: Container(width: 6, height: 6),
-                    ),
-                    Positioned(
-                      left: 21,
-                      top: 12,
-                      child: Container(
-                        width: 54,
-                        height: 21,
-                        padding: const EdgeInsets.only(
-                            top: 3, left: 11, right: 10, bottom: 3),
-                        clipBehavior: Clip.antiAlias,
-                        decoration: ShapeDecoration(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                        ),
-                        child: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Container(
-                              width: 33,
-                              height: 15,
-                              child: Column(
-                                mainAxisSize: MainAxisSize.min,
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [],
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Positioned(
-              left: 0,
-              top: 812,
-              child: Container(
-                width: 390,
-                height: 32,
-                padding: const EdgeInsets.only(
-                  top: 21,
-                  left: 125.84,
-                  right: 124.80,
-                  bottom: 6,
-                ),
-                clipBehavior: Clip.antiAlias,
-                decoration: BoxDecoration(),
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Container(
-                      width: 139.36,
-                      height: 5,
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            width: 139,
-                            height: 5,
-                            decoration: ShapeDecoration(
-                              color: Colors.black,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Positioned(
-              left: 120,
-              top: 347,
-              child: Container(
-                width: 150,
-                height: 150,
-                child: Stack(
-                  children: [
-                    Positioned(
-                      left: 0,
-                      top: 0,
-                      child: Container(
-                        width: 150,
-                        height: 150,
-                        child: Image.asset(
-                          'lib/assets/img/logo.png',
-                          width: 600.0,
-                          height: 240.0,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
-                    Positioned(
-                      left: 95.33,
-                      top: 54,
-                      child: Container(
-                        width: 8.64,
-                        height: 8.64,
-                        decoration: const ShapeDecoration(
-                          color: Colors.white,
-                          shape: OvalBorder(),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+            Center(
+            
+              child: Image.asset(
+                    'lib/assets/img/logo.png',
+                    fit: BoxFit.fill,
+                  ),
             ),
           ],
         ),
